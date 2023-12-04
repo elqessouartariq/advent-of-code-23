807e80f9aa5fc6c541b2368aab65ed4d5e9e3877
elqessouartariq <tariqelqessouar@gmail.com>
2023-12-04 13:17:10 +0100

    add day1 part2

--- day1/main.go
+++ day1/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 	"unicode"
 )
 
@@ -25,12 +26,33 @@ func main() {
 	}
 	var sum int
 
+	mapOfDigits := map[string]string{
+		"zero":"ze0ro",
+		"one":"on1e",
+		"two":"tw2o",
+		"three":"th3ree",
+		"four":"fo4ur",
+		"five":"fi5ve",
+		"six":"si6x",
+		"seven":"se7ven",
+		"eight":"ei8ght",
+		"nine":"ni9ne",
+	}
+
 	for scanner.Scan() {
-		sum	+= extractNumber(scanner.Text())			
+		sum	+= extractNumber(replaceDigits(scanner.Text(), mapOfDigits))
 	}
 
 	fmt.Println(sum)
 }
+
+func replaceDigits(line string, mp map[string]string) (string){
+	for key, value := range mp {
+		line = strings.Replace(line, key, value, -1)
+	}
+	return line
+}
+
 
 func extractNumber(line string) (int){
 	var firstNumber int
